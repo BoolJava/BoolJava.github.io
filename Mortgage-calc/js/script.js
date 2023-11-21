efd6beee0ad80f371aba1a8d0a1c0269dcaba97c
BoolJava <143815263+BoolJava@users.noreply.github.com>
2023-11-21 08:40:36 -0600

    update

--- Mortgage-calc/js/script.js
+++ Mortgage-calc/js/script.js
@@ -5,4 +5,11 @@ function calc() {
     var mnp = Number(document.getElementById("monthly pay").value);
     var n = 12
     var top = mnp * [(1 + (inr/n)) ** (n * nmy) -1]
+    var bot = irn/n
+    var res = prp * (1 + (inr/n)) ** (n * nmy)
+    var B = res - (top/bot)
+
+    var p = document.getElementById("out");
+    p.innerHTML = "";
+    p.innerHTML += "Remaining loan balance: " + B.toFixed(2) + "<br/>";
 }
