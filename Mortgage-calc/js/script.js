6cafb6f04de4dbefe1e0fa7c0c209be2a2f0bf80
BoolJava <143815263+BoolJava@users.noreply.github.com>
2023-11-27 08:36:18 -0600

    update

--- Mortgage-calc/js/script.js
+++ Mortgage-calc/js/script.js
@@ -1,8 +1,9 @@
 function calc() {
     var prp = Number(document.getElementById("principal").value);
-    var inr = Number(document.getElementById("interest rate").value);
+    var r = Number(document.getElementById("interest rate").value);
     var nmy = Number(document.getElementById("num of years").value);
     var mnp = Number(document.getElementById("monthly pay").value);
+    var inr = r / 100
     var n = 12
     var top = mnp * [(1 + (inr/n)) ** (n * nmy) -1]
     var bot = irn/n
