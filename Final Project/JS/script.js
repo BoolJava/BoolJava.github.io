700a7a87f983a53fd71702188f10ed63e1a52129
BoolJava <143815263+BoolJava@users.noreply.github.com>
2024-01-17 08:41:18 -0600

    update

--- Final Project/JS/script.js
+++ Final Project/JS/script.js
@@ -3,8 +3,10 @@ function secret() {
     var p = document.getElementById("opt");
     if (code = 53271) {
       p.innerHTML = "Nice work, hopefully you had a little fun!"
-    } else if () {
+    } else if (code < 53271 && code > 51237) {
         p.innerHTML = "Not quite but you are close"
+    } else if (code <31257) {
+
     }
 
 }
