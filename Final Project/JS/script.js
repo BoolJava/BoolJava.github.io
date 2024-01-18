ecb0a6d6f2b9b21c573af3f3b886b6c89d6ce8ba
BoolJava <143815263+BoolJava@users.noreply.github.com>
2024-01-18 08:17:32 -0600

    update

--- Final Project/JS/script.js
+++ Final Project/JS/script.js
@@ -4,9 +4,9 @@ function secret() {
     if (code = 53271) {
       p.innerHTML = "Nice work, hopefully you had a little fun!"
     } else if (code < 53271 && code > 51237) {
-        p.innerHTML = "Not quite but you are close"
+        p.innerHTML = "Not quite but you are close."
     } else if (code <31257) {
-
+      p.innerHTML = "Thats not correct."
     }
 
 }
